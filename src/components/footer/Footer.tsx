16d4b02aa9bd43c150dbf6e65490f5f60d1d7d24
jesh <jeshjangid09@gmail.com>
2024-04-08 12:39:15 +0530

    first commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Rajesh</span>
+      <span>© <a href="https://github.com/jeshjangid" target="_blank" >rajeshjangid</a> Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
